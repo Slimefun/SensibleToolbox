2e3b7e3f525ec94268118b74c5cffd87a9b25c05
Des Herriott <des.herriott@gmail.com>
2014-05-16 11:08:03 +0100

    Fix ClassNotFound problems when HoloAPI not loaded

--- src/main/java/me/desht/sensibletoolbox/util/PopupMessage.java
+++ src/main/java/me/desht/sensibletoolbox/util/PopupMessage.java
@@ -1,28 +1,18 @@
 package me.desht.sensibletoolbox.util;
 
-import com.dsh105.holoapi.HoloAPI;
-import com.dsh105.holoapi.api.Hologram;
-import com.dsh105.holoapi.api.visibility.Visibility;
 import me.desht.dhutils.MiscUtil;
 import me.desht.sensibletoolbox.SensibleToolboxPlugin;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.util.Vector;
 
 import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 public class PopupMessage {
     public static void quickMessage(Player player, Location loc, List<String> message) {
         if (SensibleToolboxPlugin.getInstance().isHoloAPIenabled()
                 && SensibleToolboxPlugin.getInstance().getConfig().getBoolean("holo_messages.enabled")) {
-            Vector v = player.getLocation().getDirection();
-            // this will place the message slightly in front of and above the location
-            v.setY(0).multiply(-0.8).add(new Vector(0.5, 0.8, 0.5));
-            double duration = SensibleToolboxPlugin.getInstance().getConfig().getDouble("holo_messages.duration_per_line") * message.size();
-            Hologram h = HoloAPI.getManager().createSimpleHologram(loc.add(v), (int)duration, message);
-            h.setVisibility(new PlayerVisibility(player));
+            HoloMessage.quickMessage(player, loc, message);
         } else {
             for (String line : message) {
                 MiscUtil.statusMessage(player, line);
@@ -32,28 +22,5 @@ public class PopupMessage {
 
     public static void quickMessage(Player player, Location loc, String... lines) {
         quickMessage(player, loc, Arrays.asList(lines));
-    }
-
-    private static class PlayerVisibility implements Visibility {
-        private final Player player;
-
-        private PlayerVisibility(Player player) {
-            this.player = player;
-        }
-
-        @Override
-        public boolean isVisibleTo(Player player, String s) {
-            return player.equals(this.player);
-        }
-
-        @Override
-        public String getSaveKey() {
-            return null;
-        }
-
-        @Override
-        public LinkedHashMap<String, Object> getDataToSave() {
-            return null;
-        }
     }
 }
