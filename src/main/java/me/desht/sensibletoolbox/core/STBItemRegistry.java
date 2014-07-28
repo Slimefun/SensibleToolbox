87d8c4610115dfb2a6d68f37909ca9c8703de2ca
Des Herriott <des.herriott@gmail.com>
2014-07-28 16:01:12 +0100

    Internal refactoring of STBItemRegistry Now caches the two constructors for an STB item when it's registered. Two benefits: a little quicker to construct items, and missing constructors are caught early on.

--- src/main/java/me/desht/sensibletoolbox/core/STBItemRegistry.java
+++ src/main/java/me/desht/sensibletoolbox/core/STBItemRegistry.java
@@ -36,7 +36,7 @@ public class STBItemRegistry implements ItemRegistry {
     public static final String LORE_PREFIX = ChatColor.DARK_GRAY.toString() + ChatColor.ITALIC + "\u25b9";
     public static final int MAX_ITEM_ID_LENGTH = 32;
 
-    private final Map<String, Class<? extends BaseSTBItem>> id2class = Maps.newHashMap();
+    private final Map<String, ReflectionDetails> reflectionDetailsMap = Maps.newHashMap();
     private final Map<String, Class<? extends BaseSTBItem>> craftingRestrictions = Maps.newHashMap();
     private final Map<String, String> permissionPrefix = Maps.newHashMap();
     private final Map<String, Plugin> id2plugin = Maps.newHashMap();
@@ -60,7 +60,19 @@ public class STBItemRegistry implements ItemRegistry {
         }
 
         Validate.isTrue(id.length() <= MAX_ITEM_ID_LENGTH, "Item ID '" + id + "' is too long! (32 chars max)");
-        id2class.put(id, item.getClass());
+        Constructor<? extends BaseSTBItem> ctor0, ctor1;
+        try {
+            ctor0 = item.getClass().getConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException("class " + item.getClass() + " does not have a 0-argument constructor!");
+        }
+        try {
+            ctor1 = item.getClass().getConstructor(ConfigurationSection.class);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException("class " + item.getClass() + " does not have a 1-argument (ConfigurationSection) constructor!");
+        }
+        ReflectionDetails details = new ReflectionDetails(item.getClass(), ctor0, ctor1);
+        reflectionDetailsMap.put(id, details);
         id2plugin.put(id, plugin);
 
         if (permissionPrefix == null) {
@@ -95,7 +107,7 @@ public class STBItemRegistry implements ItemRegistry {
 
     @Override
     public Set<String> getItemIds() {
-        return id2class.keySet();
+        return reflectionDetailsMap.keySet();
     }
 
     @Override
@@ -129,20 +141,12 @@ public class STBItemRegistry implements ItemRegistry {
 
     @Override
     public BaseSTBItem getItemById(String id, ConfigurationSection conf) {
-        Class<? extends BaseSTBItem> c = id2class.get(id);
-        if (c == null) {
+        ReflectionDetails details = reflectionDetailsMap.get(id);
+        if (details == null) {
             return null;
         }
         try {
-            BaseSTBItem item;
-            if (conf == null) {
-                Constructor<? extends BaseSTBItem> cons = c.getConstructor();
-                item = cons.newInstance();
-            } else {
-                Constructor<? extends BaseSTBItem> cons = c.getConstructor(ConfigurationSection.class);
-                item = cons.newInstance(conf);
-            }
-            return item;
+            return conf == null ? details.ctor0arg.newInstance() : details.ctor1arg.newInstance(conf);
         } catch (Exception e) {
             LogUtils.warning("failed to create STB item from item ID: " + id);
             e.printStackTrace();
@@ -166,8 +170,8 @@ public class STBItemRegistry implements ItemRegistry {
             if (!lore.isEmpty() && lore.get(0).startsWith(LORE_PREFIX)) {
                 if (c != null) {
                     Configuration conf = getItemAttributes(stack);
-                    Class<? extends BaseSTBItem> c2 = id2class.get(conf.getString("*TYPE"));
-                    return c2 != null && c.isAssignableFrom(c2);
+                    ReflectionDetails details = reflectionDetailsMap.get(conf.getString("*TYPE"));
+                    return details != null && c.isAssignableFrom(details.clazz);
                 } else {
                     return true;
                 }
@@ -200,10 +204,6 @@ public class STBItemRegistry implements ItemRegistry {
         }
     }
 
-    public String getProviderName(BaseSTBItem item) {
-        return id2plugin.get(item.getItemTypeID()).getName();
-    }
-
     public Plugin getPlugin(BaseSTBItem item) {
         return id2plugin.get(item.getItemTypeID());
     }
@@ -218,5 +218,17 @@ public class STBItemRegistry implements ItemRegistry {
 
     public void addCraftingRestriction(BaseSTBItem item, Material mat, Class<? extends BaseSTBItem> c) {
         craftingRestrictions.put(item.getItemTypeID() + ":" + mat, c);
+    }
+
+    private class ReflectionDetails {
+        private final Class<? extends BaseSTBItem> clazz;
+        private final Constructor<? extends BaseSTBItem> ctor0arg;
+        private final Constructor<? extends BaseSTBItem> ctor1arg;
+
+        private ReflectionDetails(Class<? extends BaseSTBItem> clazz, Constructor<? extends BaseSTBItem> ctor0arg, Constructor<? extends BaseSTBItem> ctor1arg) {
+            this.clazz = clazz;
+            this.ctor0arg = ctor0arg;
+            this.ctor1arg = ctor1arg;
+        }
     }
 }
