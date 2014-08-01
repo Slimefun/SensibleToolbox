d4ff8e6cb8f4319fc0bfb7e368345eb4253376ae
Des Herriott <des.herriott@gmail.com>
2014-08-01 16:48:01 +0100

    Blazes will only drop infernal dust for players with the right permission. Node "stb.craft.infernaldust" is needed.

--- src/main/java/me/desht/sensibletoolbox/listeners/MobListener.java
+++ src/main/java/me/desht/sensibletoolbox/listeners/MobListener.java
@@ -1,6 +1,7 @@
 package me.desht.sensibletoolbox.listeners;
 
 import me.desht.sensibletoolbox.SensibleToolboxPlugin;
+import me.desht.sensibletoolbox.api.items.BaseSTBItem;
 import me.desht.sensibletoolbox.items.components.InfernalDust;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
@@ -19,31 +20,34 @@ public class MobListener extends STBBaseListener {
     @EventHandler
     public void onMobDeath(EntityDeathEvent event) {
         if (event.getEntity().getType() == EntityType.BLAZE) {
-            int chance = 20, amount = 1;
-            Random r = new Random();
-            Player killer = event.getEntity().getKiller();
-            if (killer != null) {
-                ItemStack item = killer.getItemInHand();
-                if (item != null) {
-                    int level = item.getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS);
-                    switch (level) {
-                        case 1:
-                            chance = 30;
-                            amount = r.nextInt(2) + 1;
-                            break;
-                        case 2:
-                            chance = 40;
-                            amount = r.nextInt(2) + 1;
-                            break;
-                        case 3:
-                            chance = 50;
-                            amount = r.nextInt(3) + 1;
-                            break;
+            InfernalDust dust = new InfernalDust();
+            if (dust.checkPlayerPermission(event.getEntity().getKiller(), BaseSTBItem.ItemAction.CRAFT)) {
+                int chance = 20, amount = 1;
+                Random r = new Random();
+                Player killer = event.getEntity().getKiller();
+                if (killer != null) {
+                    ItemStack item = killer.getItemInHand();
+                    if (item != null) {
+                        int level = item.getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS);
+                        switch (level) {
+                            case 1:
+                                chance = 30;
+                                amount = r.nextInt(2) + 1;
+                                break;
+                            case 2:
+                                chance = 40;
+                                amount = r.nextInt(2) + 1;
+                                break;
+                            case 3:
+                                chance = 50;
+                                amount = r.nextInt(3) + 1;
+                                break;
+                        }
                     }
                 }
-            }
-            if (r.nextInt(100) < chance) {
-                event.getDrops().add(new InfernalDust().toItemStack(amount));
+                if (r.nextInt(100) < chance) {
+                    event.getDrops().add(new InfernalDust().toItemStack(amount));
+                }
             }
         }
     }
