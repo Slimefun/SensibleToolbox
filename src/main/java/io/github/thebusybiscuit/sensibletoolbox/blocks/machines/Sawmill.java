a77767c26396edb7d288442f907117a928125ed5
IAmSorry <37082875+IAmSorryArt@users.noreply.github.com>
2020-10-03 18:38:27 +0200

    Update Sawmill.java

--- src/main/java/io/github/thebusybiscuit/sensibletoolbox/blocks/machines/Sawmill.java
+++ src/main/java/io/github/thebusybiscuit/sensibletoolbox/blocks/machines/Sawmill.java
@@ -91,7 +91,7 @@ public class Sawmill extends AbstractIOMachine {
     @Override
     protected void onMachineStartup() {
         if (SensibleToolbox.getPluginInstance().getConfigCache().isNoisyMachines()) {
-            getLocation().getWorld().playSound(getLocation(), Sound.ENTITY_HORSE_STEP_WOOD, 1.0f, 0.5f);
+            getLocation().getWorld().playSound(getLocation(), Sound.ENTITY_HORSE_STEP_WOOD, 1.0F, 0.5F);
         }
     }
 
