a92c319a6b224d2e891c6c7fd46340b4749a0f62
IAmSorry <37082875+IAmSorryArt@users.noreply.github.com>
2020-10-03 21:12:00 +0200

    Update BaseSTBMachine.java

--- src/main/java/io/github/thebusybiscuit/sensibletoolbox/api/items/BaseSTBMachine.java
+++ src/main/java/io/github/thebusybiscuit/sensibletoolbox/api/items/BaseSTBMachine.java
@@ -798,7 +798,8 @@ public abstract class BaseSTBMachine extends BaseSTBBlock implements ChargeableB
             int i = 0;
 
             for (int slot : getOutputSlots()) {
-                inv.setItem(i++, getInventoryItem(slot));
+                i++;
+                inv.setItem(i, getInventoryItem(slot));
             }
 
             return inv;
@@ -814,7 +815,8 @@ public abstract class BaseSTBMachine extends BaseSTBBlock implements ChargeableB
             int i = 0;
 
             for (int slot : getOutputSlots()) {
-                setInventoryItem(slot, inventory.getItem(i++));
+                i++;
+                setInventoryItem(slot, inventory.getItem(i));
             }
         }
     }
