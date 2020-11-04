92a60ebd59ae0f2e1c3291a4efae992e7c3c87ff
RockTheDev <37476299+RockTheDev@users.noreply.github.com>
2020-11-04 17:26:51 +0200

    Fixed building issue
    Fixed building issue - missed an import

--- src/main/java/io/github/thebusybiscuit/sensibletoolbox/blocks/machines/InfernalFarm.java
+++ src/main/java/io/github/thebusybiscuit/sensibletoolbox/blocks/machines/InfernalFarm.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Ageable;
