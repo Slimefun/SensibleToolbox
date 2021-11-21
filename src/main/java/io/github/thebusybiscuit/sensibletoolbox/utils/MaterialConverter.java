1c1e04c385a1722d9f9ae45cd1df965f9610ca70
BuildTools <unconfigured@null.spigotmc.org>
2021-11-21 10:09:01 +0100

    Docs

--- src/main/java/io/github/thebusybiscuit/sensibletoolbox/utils/MaterialConverter.java
+++ src/main/java/io/github/thebusybiscuit/sensibletoolbox/utils/MaterialConverter.java
@@ -6,14 +6,16 @@ import org.bukkit.Material;
 
 import lombok.NonNull;
 
+/**
+ * A collection of miscellaneous material-related utility methods.
+ */
 public final class MaterialConverter {
 	/**
-     * Turn log into sapling preserving tree type
-     *
-     * @param log
-     *           log  you want to turn into sapling
-     * @return sapling
-     */
+	 * Turn log into sapling preserving tree type
+	 *
+	 * @param log log you want to turn into sapling
+	 * @return sapling
+	 */
 	public static Optional<Material> getSaplingFromLog(@NonNull Material log) {
 		if (!isLog(log))
 			return Optional.empty();
@@ -26,13 +28,13 @@ public final class MaterialConverter {
 			return Optional.empty();
 		}
 	}
+
 	/**
-     * Turn log into planks preserving tree type
-     *
-     * @param log
-     *           log you want to turn into planks
-     * @return planks
-     */
+	 * Turn log into planks preserving tree type
+	 *
+	 * @param log log you want to turn into planks
+	 * @return planks
+	 */
 	public static Optional<Material> getPlanksFromLog(@NonNull Material log) {
 		if (!isLog(log))
 			return Optional.empty();
@@ -45,13 +47,13 @@ public final class MaterialConverter {
 			return Optional.empty();
 		}
 	}
+
 	/**
-     * Check if material is log (any type)
-     *
-     * @param log
-     *            the material to check
-     * @return true if the stack is log; false otherwise
-     */
+	 * Check if material is log (any type)
+	 *
+	 * @param log the material to check
+	 * @return true if the stack is log; false otherwise
+	 */
 	public static boolean isLog(@NonNull Material log) {
 		return log.name().endsWith("_LOG") || log.name().endsWith("_WOOD");
 	}
