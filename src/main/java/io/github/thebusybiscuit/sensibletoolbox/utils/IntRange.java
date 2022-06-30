184b0a8e1acc9a36f599c3b6d46fc91ef54d9198
Bill <bunnkywhat@gmail.com>
2022-06-30 06:16:25 -0400

    Suggested fixes; Removed unused

--- src/main/java/io/github/thebusybiscuit/sensibletoolbox/utils/IntRange.java
+++ src/main/java/io/github/thebusybiscuit/sensibletoolbox/utils/IntRange.java
@@ -6,12 +6,10 @@ public class IntRange {
     private final int max;
 
     public IntRange(int number) {
-        this.min = number;
-        this.max = number;
+        this(number, number);
     }
 
     public IntRange(int number1, int number2) {
-        super();
         if (number2 < number1) {
             this.min = number2;
             this.max = number1;
@@ -25,28 +23,11 @@ public class IntRange {
     // Accessors
     ///////////////////////////
 
-    public long getMinimumLong() { return min; }
-    public long getMaximumLong() { return max; }
     public int getMinimumInt() { return min; }
     public int getMaximumInt() { return max; }
 
-    public boolean containsNumber(Number number) {
-        if (number == null) {
-            return false;
-        }
-        return containsInteger(number.intValue());
-    }
 
     public boolean containsInteger(int value) {
         return value >= min && value <= max;
-    }
-
-    public int[] toArray() {
-        int[] array = new int[max - min + 1];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = min + i;
-        }
-
-        return array;
     }
 }
